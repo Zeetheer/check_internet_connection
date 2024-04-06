70c7ed19b1fd757c2529a5049660dee2556c8dcb
Snaplan <81860686+Snaplan@users.noreply.github.com>
2024-04-07 00:23:09 +0900

    bug fix

--- lib/src/check_internet_connection_base.dart
+++ lib/src/check_internet_connection_base.dart
@@ -1,14 +1,12 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
-
 Future<void> checkInternectConnectionWithFunctions(
     Function ifNotNone, Function ifNone) async {
-  final List<ConnectivityResult> connectivityResult =
+  final ConnectivityResult connectivityResult =
       await (Connectivity().checkConnectivity());
-  if (!connectivityResult.contains(ConnectivityResult.none)) {
+  if (connectivityResult != (ConnectivityResult.none)) {
     ifNotNone();
-  } else if (connectivityResult.contains(ConnectivityResult.none)) {
+  } else if (connectivityResult == (ConnectivityResult.none)) {
     ifNone();
   }
 }
-
